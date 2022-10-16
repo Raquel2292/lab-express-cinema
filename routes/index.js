6c925745c4817e112da2ef716d025a81369df78b
Raquel Blázquez <raquel.blazquez.estrada@gmail.com>
2022-10-16 16:04:10 +0200

    done!

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const Cinema = require('../models/Movie.model');
+
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+router.get ("/movies", (req, res, next) => {
+    Cinema.find()
+    .then((moviesList) => {
+     res.render("movies.hbs", {
+        moviesList
+     })
+})
+.catch((err) => {
+    next(err);
+})
+})
+
+
+router.get("/movies/:moviedetails", (req, res, next) => {
+    const { moviedetails } = req.params;
+    Cinema.findById(moviedetails)
+    //console.log(moviedetails)
+    .then((response) => {  
+        console.log(response)
+        res.render("movie-details.hbs", {
+            movieDetails: response
+        })
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
 
 module.exports = router;
